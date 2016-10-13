8dc918f90014735761af8071b0a374abdfe95962
TejaYenduri <teja.rithu@gmail.com>
2016-10-13 14:19:03 -0700

    Removed a file

--- hashPattern.py
+++ hashPattern.py
@@ -1,3 +1,12 @@
+#Write a script to print output in the below format
+#If the input is  num = 5
+#
+     #
+    ###
+   #####
+  #######
+ #########
+#
 num = input("Enter a number ")
 
 for i in range(num) :
